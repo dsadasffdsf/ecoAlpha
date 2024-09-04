0b3e56aa0543cfdd129bf44466dc960e7cc8138c
dsadasffdsf <qwertfdsazxcvg122@mail.ru>
2024-09-05 02:14:23 +0300

    qwe

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './src/App';
 import './styles/style.css';
-import { store } from './redux/store';
+import { store } from './src/redux/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
